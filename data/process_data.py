0fb6fc67f87e4175e8ead16dd6183435f3192d03
Mnegh <mohammed.negheimish@gmail.com>
2020-06-17 02:46:49 +0300

    Fixed non-binary elements

--- data/process_data.py
+++ data/process_data.py
@@ -6,20 +6,20 @@ from sqlalchemy import create_engine
 def load_data(messages_filepath, categories_filepath):
     # load messages dataset
     messages = pd.read_csv(messages_filepath)
-    
-    
+
+
     # load categories dataset
     categories = pd.read_csv(categories_filepath)
-    
+
     # dropping duplicates
     messages.drop_duplicates(subset=['id'],inplace=True)
     categories.drop_duplicates(subset=['id'],inplace=True)
-    
+
     # merging data based on id
     df = pd.merge(messages,categories, how='inner',left_on=['id'],right_on=['id'])
-    
+
     return df, categories
- 
+
 def clean_data(df, categories):
     # creates a dataframe of the 36 individual categories columns
     categories_split = pd.DataFrame(categories['categories'].str.split(';',expand=True))
@@ -29,32 +29,34 @@ def clean_data(df, categories):
     category_colnames = row.apply(lambda x: x.partition('-')[0])
     # rename the categories column
     categories_split.columns = category_colnames
-    
+
     # convert the label values to binary
     for column in categories_split:
         # set each value to be the last character of the string
         categories_split[column] = categories_split[column].apply(lambda x: x.partition('-')[-1])
-
+        # change the values from the column that is not binary to 1
+        categories_split[column] = categories_split[column].apply(lambda x: x if x in [0,1] else 1)
         # convert column from string to numeric
         categories_split[column] = categories_split[column].apply(lambda x: int(x))
-    
+
+
     # drop the original categories column from `df`
     df.drop(columns = ['categories'],inplace=True)
-    
+
     # resetting indices to avoid NaN bug in concatenation
     df.reset_index(drop=True, inplace=True)
     categories_split.reset_index(drop=True, inplace=True)
-    
+
     # concatenate the original df with the new cateogries dataframe
     df = pd.concat([df,categories_split],axis=1)
-    
+
     return df
-    
+
 
 
 def save_data(df, database_filename):
     engine = create_engine('sqlite:///'+database_filename)
-    df.to_sql('Data', engine, index=False)
+    df.to_sql('Data', engine, index=False,if_exists='replace')
 
 def main():
     if len(sys.argv) == 4:
@@ -67,12 +69,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df, categories)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
